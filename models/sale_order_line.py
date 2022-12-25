e45b9b607c80ce3a3486920f124c37a6084a0147
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-12-25 22:35:28 +0100

    Sale Order:Display warning message that the Prepress Proof of the product selected has already been quarantined

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -7,12 +7,14 @@ from odoo.exceptions import ValidationError
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    prepress_proof_id = fields.Many2one('prepress.proof',string='Prepress proof',readonly=True)
+    prepress_proof_id = fields.Many2one('prepress.proof', string='Prepress proof', readonly=True)
 
     @api.onchange('product_id')
     def product_id_change(self):
         res = super(SaleOrderLine, self).product_id_change()
-        self._update_prepress_proof()
+        warning = self._update_prepress_proof()
+        if warning:
+            return warning
         return res
 
     def _update_prepress_proof(self):
@@ -20,6 +22,14 @@ class SaleOrderLine(models.Model):
             return
         prepress_proof = self.env['prepress.proof']._get_by_product(self.product_id)
         self.update({'prepress_proof_id': prepress_proof and prepress_proof.id or False})
+        if prepress_proof.quarantined:
+            warning = {
+                'title': _("Warning"),
+                'message': _(
+                    "Prepress Proof %s has already been quarantined!"
+                ) % prepress_proof.name,
+            }
+            return {'warning': warning}
 
     def write(self, vals):
         res = super(SaleOrderLine, self).write(vals)
@@ -27,13 +37,3 @@ class SaleOrderLine(models.Model):
             if each.state == 'sale' and each.product_id.type == 'product' and not each.prepress_proof_id:
                 raise ValidationError(_("The Prepress proof is required in sale state!"))
         return res
-
-
-
-
-
-
-
-
-
-
