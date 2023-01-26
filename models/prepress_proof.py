8dcba4aade3270743faca217c9aa8a63e66350f2
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-01-26 10:52:50 +0100

    Sale Order:get the validated/flashed Prepress proofs first in order lines

--- models/prepress_proof.py
+++ models/prepress_proof.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+from odoo.osv.expression import OR
 
 
 class PrepressProof(models.Model):
@@ -11,13 +12,23 @@ class PrepressProof(models.Model):
     @api.model
     def _get_by_product(self,product_id):
         """ Get the Prepress proof by specified product"""
-        domain = self._get_accepted_prepress_proof_domain()
+        # we have to search the validated/flashed prepress_proof first and only if we have not found we search the In progress ones
+        domain = self._get_favorite_prepress_proof_domain()
         domain.append(('product_id','=',product_id.id))
-        return self.search(domain,limit=1)
+        prepress_proof = self.search(domain,limit=1)
+        if not prepress_proof:
+            domain = self._get_accepted_prepress_proof_domain()
+            domain.append(('product_id', '=', product_id.id))
+            prepress_proof = self.search(domain,limit=1)
+        return prepress_proof
 
 
+    def _get_favorite_prepress_proof_domain(self):
+        favorite_states = ('validated','flashed')
+        return [('state','in',favorite_states)]
 
     @api.model
     def _get_accepted_prepress_proof_domain(self):
-        accepted_states = ('in_progress','validated','flashed')
-        return [('state','in',accepted_states)]
+        accepted_states = ('in_progress',)
+        accepted_domain = [('state','in',accepted_states)]
+        return OR([self._get_favorite_prepress_proof_domain(),accepted_domain])
