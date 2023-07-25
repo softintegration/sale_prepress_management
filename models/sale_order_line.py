2bc41b9b223467bd565cbbfd430828f04f9b92f0
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-07-25 14:32:49 +0100

    Prepress proof:Add constraint to prevent deletion of Proof related to Sale order

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -7,7 +7,7 @@ from odoo.exceptions import ValidationError
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    prepress_proof_id = fields.Many2one('prepress.proof', string='Prepress proof', readonly=True)
+    prepress_proof_id = fields.Many2one('prepress.proof', string='Prepress proof', readonly=True,ondelete="restrict")
     prepress_proof_client_ref = fields.Char(string='Customer Prepress proof reference',related='prepress_proof_id.client_ref')
 
     @api.onchange('product_id')
